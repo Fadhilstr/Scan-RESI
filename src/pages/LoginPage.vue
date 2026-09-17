10c902d5e5e93aebf9a30fa6f2c47e0f02546a62
Fadhil <2111500233@student.budiluhur.ac.id>
2026-09-17 11:37:42 +0700

    fix: update username placeholder and correct form tag in LoginPage

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -23,7 +23,7 @@
                 v-model="username"
                 outlined
                 label="Username / Email"
-                placeholder="cth: fadhil, admin"
+                placeholder="cth: ******* "
                 autocomplete="username"
                 autofocus
                 :rules="[v => !!v || 'Username wajib diisi']"
@@ -176,7 +176,7 @@
               <div class="row justify-center q-mt-sm">
                 <q-btn flat dense no-caps color="grey-7" icon="arrow_back" label="Kembali ke Login" @click="cancelForgot" />
               </div>
-            </q-form>
+            </q-form>f
           </div>
 
           <!-- STEP 4: LUPA PASSWORD — VERIFIKASI OTP RESET -->
